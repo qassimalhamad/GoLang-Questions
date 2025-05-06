74e6d691023dce09772873c6c1deb5cdb8ab48b0
qalhamad <qalhamad33@gmail.com>
2025-05-06 15:05:13 +0300

    lastword

---
+++ lastword.go
@@ -0,0 +1,16 @@
+package piscine
+
+func LastWord(s string) string {
+	result := ""
+
+	for i := len(s) - 1; i > 0; i-- {
+		if s[i] == ' ' {
+			if result != "" {
+				break
+			}
+		} else {
+			result = string(s[i]) + result
+		}
+	}
+	return result + "\n"
+}
